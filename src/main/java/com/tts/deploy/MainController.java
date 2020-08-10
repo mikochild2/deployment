e1dbf6452c0f8087e42e68f197575d1165fd9fa9
mikochild2 <mikochild2@gmail.com>
2020-08-10 08:46:59 -0400

    first commit

---
+++ src/main/java/com/tts/deploy/MainController.java
@@ -0,0 +1,16 @@
+package com.tts.deploy;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "hello world!";
+    }
+
+
+
+}
